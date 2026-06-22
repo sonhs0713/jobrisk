6fe3ca301282bf70b083b9e1e855b62624ef16a4
Hyeonsu Son <sonhs0713@gmail.com>
2026-06-22 15:20:08 +0900

    Add Microsoft Clarity tracking script

--- web/app/layout.js
+++ web/app/layout.js
@@ -46,6 +46,15 @@ export default function RootLayout({ children }) {
             fbq('track', 'PageView');
           `}
         </Script>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "wfjp3gi0kf");
+          `}
+        </Script>
       </head>
       <body>
         <noscript>
