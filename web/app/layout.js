79ad6f06f4a05d371a70d8bc88c954260a67d108
Hyeonsu Son <sonhs0713@gmail.com>
2026-07-14 14:00:59 +0900

    feat: switch layout tracking to GA4

--- web/app/layout.js
+++ web/app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
 import Script from 'next/script'
 
-const GTM_ID = 'GTM-563WXF8N'
+const GA_MEASUREMENT_ID = 'G-0CCNPQS627'
 
 export const metadata = {
   metadataBase: new URL('https://jobrisk.vercel.app'),
@@ -34,13 +34,16 @@ export default function RootLayout({ children }) {
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/variable/pretendardvariable.css"
         />
-        <Script id="google-tag-manager" strategy="beforeInteractive">
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
           {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','${GTM_ID}');
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}');
           `}
         </Script>
         <Script id="meta-pixel" strategy="afterInteractive">
@@ -68,15 +71,6 @@ export default function RootLayout({ children }) {
         </Script>
       </head>
       <body>
-        <noscript>
-          <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-            title="google-tag-manager"
-          />
-        </noscript>
         <noscript>
           <img
             height="1"
